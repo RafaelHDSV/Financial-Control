2cb1da5fa73b64299b7563c54445ef02c718d770
RafaelHDSV <rafaebolt@gmail.com>
2022-12-29 10:28:02 -0300

    Adding form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import GlobalStyle from './styles/global'
 import Header from './components/header'
 import Resume from './components/resume'
+import Form from './components/form'
 
 const App = () => {
     return (
         <>
             <Header />
             <Resume />
+            <Form />
             <GlobalStyle />
         </>
     )
