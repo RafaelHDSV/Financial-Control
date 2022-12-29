870cda9e3b2ae8adc0e41e34ef96476eb299fd42
RafaelHDSV <rafaebolt@gmail.com>
2022-12-29 10:21:20 -0300

    Updating resume itens

--- src/components/resumeItem/style.js
+++ src/components/resumeItem/style.js
@@ -32,10 +32,11 @@ export const Header = styled.header`
     justify-content: space-around;
     width: 100%;
     gap: 10px;
+    margin: 20px auto;
 
     svg {
         width: 25px;
-        height: 25px;        
+        height: 25px;
     }
 `
 export const HeaderTitle = styled.p`
