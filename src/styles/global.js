53b167b5f41981c8bab2c0f16fa056647303942c
RafaelHDSV <rafaebolt@gmail.com>
2022-12-29 09:41:20 -0300

    Adding global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,1 +1,12 @@
 import { createGlobalStyle } from 'styled-components'
+
+const Global = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Poppins', Sans-serif;
+        background-color: #f2f2f2;
+    }
+`;
+
+export default Global
