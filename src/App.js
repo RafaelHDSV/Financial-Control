928fe6834df5c4a134aa2c5eee7cd6cc2a7dac70
RafaelHDSV <rafaebolt@gmail.com>
2022-12-29 09:52:44 -0300

    Adding header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import GlobalStyle from './styles/global'
+import Header from './components/header'
 
 const App = () => {
     return (
-        <div>App</div>
+        <>
+            <Header />
+            <GlobalStyle />
+        </>
     )
 }
 
